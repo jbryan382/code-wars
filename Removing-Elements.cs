30a8608abd8e4f151be1f36499133600ade47f60
Jordan Bryan <46909427+jbryan382@users.noreply.github.com>
2020-08-26 17:02:58 -0400

    Walkthrough Linq with student

---
+++ Removing-Elements.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+  
+public static class Kata
+    {
+        public static object[] RemoveEveryOther(object[] arr)
+        {
+          return arr.Where((element, index) => index % 2 == 0).ToArray();
+        }
+    }
