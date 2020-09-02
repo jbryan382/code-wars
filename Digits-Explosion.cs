53ae3f952bdbfd889ae4e9ab1204dda2250a6c50
Jordan Bryan <46909427+jbryan382@users.noreply.github.com>
2020-09-02 14:33:52 -0400

    Commented Instructions

--- Digits-Explosion.cs
+++ Digits-Explosion.cs
@@ -5,17 +5,23 @@ public class Digits
 {
   public static string Explode(string s)
   {
+   // Creating a string to hold our exploding digits
     var expString = "";
+   // I want to loop through the length of the string to alter each character
     for (var i = 0; i < s.Length; i++)
     {
+   // Creating numChar to hold the char value of the variable
       var numChar = s[i].ToString();
+   // Creating num to hold the int value of the variable
       var num = Int32.Parse(numChar);
-      
+   // Dependent on the number I would like to loop through and duplicate the number num number of times.
       for(var j = 0; j < num; j++)
       {
+       // Each step of the way the character is added to our return string
        expString += numChar; 
       }
     }
+    // Return the new exploded string to the system
     return expString;
   }
 }
