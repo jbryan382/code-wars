a133036136a36464c64f5d9b76a7f0a6858b914c
Jordan Bryan <46909427+jbryan382@users.noreply.github.com>
2020-09-17 15:46:34 -0400

    Refactored to use concat

--- Moving-Zeros.js
+++ Moving-Zeros.js
@@ -1,9 +1,5 @@
 var moveZeros = function (arr) {
-  if(arr){
   let theRest = arr.filter(element => element !== 0)
   let zeroes = arr.filter(element => element === 0)
-  zeroes.forEach(z => theRest.push(z))
-  return theRest
+  return theRest.concat(zeroes)
   }
-  return arr
-}
