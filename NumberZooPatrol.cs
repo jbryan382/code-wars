a25b75886639940590bfdf1e4254a904060908af
Jordan Bryan <46909427+jbryan382@users.noreply.github.com>
2021-05-04 03:32:26 -0400

    quick comment edit

--- NumberZooPatrol.cs
+++ NumberZooPatrol.cs
@@ -6,7 +6,7 @@ public class Kata
   {
 //     Finding n or the length of the array + 1 accounting for the 0 index to find ASSUMED total
     var highestInt = array.Length + 1;
-//     Utilizing the factorial equation for finding the facotrial of the ASSUMED maximun INT of the array given
+//     Utilizing the factorial equation for finding the facotrial of the ASSUMED maximun sum of the array given
     var assumedSumOfAllNumbers = highestInt * (highestInt + 1) / 2;
 //     return the difference between the ASSUMED sum of the array and the sum (or the factorial)
 //     of the given array to find the missing int
